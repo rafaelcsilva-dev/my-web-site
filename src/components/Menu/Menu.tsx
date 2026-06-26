4fa5cf1f546847b6e0a6bc64a45f28b4c7760270
Rafael da Costa Silva <117859131+RafaelCostaSIlva1501@users.noreply.github.com>
2026-06-26 19:18:14 -0300

    menu wrapper

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -10,81 +10,86 @@ export default function Menu() {
   const { activeTheme, changePalette } = useAppTheme();
 
   return (
-    <aside className="menu">
-      <header>
-        <span>// MENU_ORCHESTRATOR</span>
-      </header>
+    <aside className="container-menu">
+      <div className="menu-content">
+        <header>
+          <span>// MENU_ORCHESTRATOR</span>
+        </header>
 
-      <section>
-        <button
-          className={
-            location.pathname === "/" || location.pathname === "/home"
-              ? "nav-active"
-              : ""
-          }
-          onClick={() => navigate("/")}
-        >
-          [ HOME ]
-        </button>
-        <button
-          className={location.pathname === "/projects" ? "nav-active" : ""}
-          onClick={() => navigate("/projects")}
-        >
-          [ PROJECTS ]
-        </button>
-        <button
-          className={
-            location.pathname === "/about" || location.pathname === "/sobre"
-              ? "nav-active"
-              : ""
-          }
-          onClick={() => navigate("/about")}
-        >
-          [ ABOUT ]
-        </button>
+        <section>
+          <button
+            className={
+              location.pathname === "/" || location.pathname === "/home"
+                ? "nav-active"
+                : ""
+            }
+            onClick={() => navigate("/")}
+          >
+            [ HOME ]
+          </button>
+          <button
+            className={location.pathname === "/projects" ? "nav-active" : ""}
+            onClick={() => navigate("/projects")}
+          >
+            [ PROJECTS ]
+          </button>
+          <button
+            className={
+              location.pathname === "/about" || location.pathname === "/sobre"
+                ? "nav-active"
+                : ""
+            }
+            onClick={() => navigate("/about")}
+          >
+            [ ABOUT ]
+          </button>
 
-        <hr />
+          <hr />
 
-        <button
-          className={
-            location.pathname === "/articles" ||
-            location.pathname === "/artigos"
-              ? "nav-active"
-              : ""
-          }
-          onClick={() => navigate("/articles")}
-        >
-          [ ARTICLES ]
-        </button>
-        <button
-          className={
-            location.pathname === "/studies" || location.pathname === "/estudos"
-              ? "nav-active"
-              : ""
-          }
-          onClick={() => navigate("/studies")}
-        >
-          [ STUDIES ]
-        </button>
-      </section>
+          <button
+            className={
+              location.pathname === "/articles" ||
+              location.pathname === "/artigos"
+                ? "nav-active"
+                : ""
+            }
+            onClick={() => navigate("/articles")}
+          >
+            [ ARTICLES ]
+          </button>
+          <button
+            className={
+              location.pathname === "/studies" ||
+              location.pathname === "/estudos"
+                ? "nav-active"
+                : ""
+            }
+            onClick={() => navigate("/studies")}
+          >
+            [ STUDIES ]
+          </button>
+        </section>
 
-      <footer>
-        <span>PALLETE</span>
+        <footer>
+          <span>PALLETE</span>
 
-        <div className="palette-container">
-          {["cyber", "matrix", "amber", "space", "steel", "virus"].map(
-            (theme) => (
-              <button
-                key={theme}
-                className={activeTheme === theme ? "btn-active" : ""}
-                onClick={() => changePalette(theme)}
-              >
-                {theme.toUpperCase()}
-              </button>
-            ),
-          )}
-        </div>
-      </footer>
+          <div className="palette-container">
+            {["cyber", "matrix", "amber", "space", "steel", "virus"].map(
+              (theme) => (
+                <button
+                  key={theme}
+                  className={activeTheme === theme ? "btn-active" : ""}
+                  onClick={() => changePalette(theme)}
+                >
+                  {theme.toUpperCase()}
+                </button>
+              ),
+            )}
+          </div>
+        </footer>
+      </div>
+
+      <div className="menu-spacer"></div>
     </aside>
   );
 }
